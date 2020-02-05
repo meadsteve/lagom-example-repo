449de699d978fb2a3b620c9f58513b896301a3bb
steve <meadsteve@gmail.com>
2020-02-05 08:49:18 +0100

    switch the greetings to be defined by a regular function

--- entry.py
+++ entry.py
@@ -1,17 +1,23 @@
 from typing import List
 
 from lagom import Container
+from lagom.decorators import dependency_definition
 from starlette.applications import Starlette
 from starlette.routing import Route
 
 from example.homepage import homepage_handler, Greeting
 
 container = Container()
-container[List[Greeting]] = [
-    Greeting("Hello"),
-    Greeting("Hej"),
-    Greeting("Hellå")
-]
+
+
+@dependency_definition(container, singleton=True)
+def greetings() -> List[Greeting]:
+    return [
+        Greeting("Hello"),
+        Greeting("Hej"),
+        Greeting("Hellå")
+    ]
+
 
 app = Starlette(debug=True, routes=[
     Route('/', container.partial(homepage_handler)),
